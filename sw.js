f2119d819337d88011cc3d6722ea5c351ee1bf3f
chanyoung1129-arch <chanyoung1129@gmail.com>
2026-05-29 12:06:42 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'church-attendance-v1';
+const assets = [
+  '출석부_앱.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
